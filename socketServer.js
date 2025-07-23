eb1140ac0a6af1fbf4849bf22e077d2bbc884fe1
Saugat Lamichhane <lamichhanesaugat62@gmail.com>
2025-07-23 15:26:03 +0545

    redundant to

--- socketServer.js
+++ socketServer.js
@@ -189,7 +189,6 @@ export default function initSocketServer(httpServer) {
     if (!challenge || challenge.status !== "in_progress") return;
 
     // Check if move is valid
-    const posStr = `${to.row}-${to.col}`;
     const alreadyOccupied = board.goats.concat(board.tigers).some(p => p.row === to.row && p.col === to.col);
     const goatLimitReached = board.goats.length >= 20;
 
